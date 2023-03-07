7dbfc6982affac8dcb77b5249c6555a552daa49c
YusukeIwaki <iwaki@i3-systems.com>
2023-03-07 23:28:45 +0900

    Add test for creating a factory class with another name.

---
+++ test/data_class_factory_test.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'data_class_factory'
+
+class DataTest < Minitest::Test
+  MyDataClass = DataClassFactory.define_factory_class
+  def test_factory_with_another_name
+    klass = MyDataClass.define(:foo, :bar)
+    value = klass.new(foo: 1, bar: 2)
+    assert_equal 1, value.foo
+    assert_equal 2, value.bar
+  end
+end
